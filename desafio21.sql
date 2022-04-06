bac0ad8895b9ddcc9ea7e5e396e6d55b014bb547
kelsonbatista <kelsonbatista@gmail.com>
2022-04-05 21:00:37 -0300

    Requisito 21

---
+++ desafio21.sql
@@ -0,0 +1,5 @@
+-- 21 - Adicione com um único INSERT, duas linhas à tabela order_details com os mesmos dados do requisito 20.
+-- SELECT COUNT(*) FROM order_details;
+INSERT INTO order_details VALUES
+(NULL, 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
+(NULL, 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
